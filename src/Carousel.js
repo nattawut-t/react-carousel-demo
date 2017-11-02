0aae8adc8afd8d6d87479e8bb59b062cd6b830db
j2sdk_ball2 <codecrazii@gmail.com>
2017-11-02 16:47:54 +0700

    initial carousel in componentDidMount()

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,68 +1,77 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-const Carousel = () =>
-  <div className="container main-container">
+class Carousel extends Component {
+  componentDidMount() {
+    const $myCarousel = $('#carousel-example-generic')
+    const $firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']")
 
-    <h1>Bootstrap Carousel with Animate.css</h1>
-    <div id="carousel-example-generic" className="carousel slide">
+    // console.log('firstAnimatingElems: ', $firstAnimatingElems)
 
-      <ol className="carousel-indicators">
-        <li data-target="#carousel-example-generic" data-slide-to="0" className="active" />
-        <li data-target="#carousel-example-generic" data-slide-to="1" />
-        <li data-target="#carousel-example-generic" data-slide-to="2" />
-      </ol>
+    $firstAnimatingElems.carousel()
+  }
 
-      <div className="carousel-inner" role="listbox">
+  render() {
+    return (
+      <div className="container main-container">
+        <h1>Bootstrap Carousel with Animate.css</h1>
+        <div id="carousel-example-generic" className="carousel slide">
+          <ol className="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" className="active" />
+            <li data-target="#carousel-example-generic" data-slide-to="1" />
+            <li data-target="#carousel-example-generic" data-slide-to="2" />
+          </ol>
+          <div className="carousel-inner" role="listbox">
+            <div className="item active deepskyblue">
+              <div className="carousel-caption">
+                <h3 data-animation="animated bounceInLeft">
+                  This is the caption for slide 1
+                </h3>
+                <h3 data-animation="animated bounceInRight">
+                  This is the caption for slide 1
+                </h3>
+                <button className="btn btn-primary btn-lg" data-animation="animated zoomInUp">Button</button>
+              </div>
+            </div>
 
-        <div className="item active deepskyblue">
+            <div className="item skyblue">
+              <div className="carousel-caption">
+                <h3 className="icon-container" data-animation="animated bounceInDown">
+                  <span className="glyphicon glyphicon-heart" />
+                </h3>
+                <h3 data-animation="animated bounceInUp">
+                  This is the caption for slide 2
+                </h3>
+                <button className="btn btn-primary btn-lg" data-animation="animated zoomInRight">Button</button>
+              </div>
+            </div>
 
-          <div className="carousel-caption">
-            <h3 data-animation="animated bounceInLeft">
-              This is the caption for slide 1
-            </h3>
-            <h3 data-animation="animated bounceInRight">
-              This is the caption for slide 1
-            </h3>
-            <button className="btn btn-primary btn-lg" data-animation="animated zoomInUp">Button</button>
+            <div className="item darkerskyblue">
+              <div className="carousel-caption">
+                <h3 className="icon-container" data-animation="animated zoomInLeft">
+                  <span className="glyphicon glyphicon-glass" />
+                </h3>
+                <h3 data-animation="animated flipInX">
+                  This is the caption for slide 3
+                </h3>
+                <button className="btn btn-primary btn-lg" data-animation="animated lightSpeedIn">Button</button>
+              </div>
+            </div>
+
           </div>
-        </div>
 
-        <div className="item skyblue">
-          <div className="carousel-caption">
-            <h3 className="icon-container" data-animation="animated bounceInDown">
-              <span className="glyphicon glyphicon-heart" />
-            </h3>
-            <h3 data-animation="animated bounceInUp">
-              This is the caption for slide 2
-            </h3>
-            <button className="btn btn-primary btn-lg" data-animation="animated zoomInRight">Button</button>
-          </div>
-        </div>
-
-        <div className="item darkerskyblue">
-          <div className="carousel-caption">
-            <h3 className="icon-container" data-animation="animated zoomInLeft">
-              <span className="glyphicon glyphicon-glass" />
-            </h3>
-            <h3 data-animation="animated flipInX">
-              This is the caption for slide 3
-            </h3>
-            <button className="btn btn-primary btn-lg" data-animation="animated lightSpeedIn">Button</button>
-          </div>
+          <a className="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+            <span className="glyphicon glyphicon-chevron-left" aria-hidden="true" />
+            <span className="sr-only">Previous</span>
+          </a>
+          <a className="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+            <span className="glyphicon glyphicon-chevron-right" aria-hidden="true" />
+            <span className="sr-only">Next</span>
+          </a>
         </div>
 
       </div>
-
-      <a className="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-        <span className="glyphicon glyphicon-chevron-left" aria-hidden="true" />
-        <span className="sr-only">Previous</span>
-      </a>
-      <a className="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-        <span className="glyphicon glyphicon-chevron-right" aria-hidden="true" />
-        <span className="sr-only">Next</span>
-      </a>
-    </div>
-
-  </div>
+    )
+  }
+}
 
 export default Carousel
