e53d17abebd4a127da310187a6156c16f25f0427
j2sdk_ball2 <codecrazii@gmail.com>
2017-11-02 17:00:23 +0700

    link custom css in index

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { render } from 'react-dom'
+
+// not working
+// import 'bootstrap/dist/css/bootstrap.css'
+
 import Carousel from './Carousel'
+import '../assets/css/carousel.css'
 
 // const Root = () => ''
 
